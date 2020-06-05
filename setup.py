fd148d481c764a491246d2b51d64e201b897c80e
RemixDev <RemixDev64@gmail.com>
2020-06-05 10:49:05 +0200

    Updated PyPi to 1.0.12

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="deemix",
-    version="1.0.11",
+    version="1.0.12",
     description="A barebone deezer downloader library",
     long_description=README,
     long_description_content_type="text/markdown",
