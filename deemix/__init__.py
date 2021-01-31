e88d05015a9d630e50adb8f87d69f849e98f4738
RemixDev <RemixDev64@gmail.com>
2021-01-31 20:46:45 +0100

    Version bump to 2.0.12

--- deemix/__init__.py
+++ deemix/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-__version__ = "2.0.11"
+__version__ = "2.0.12"
 USER_AGENT_HEADER = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) " \
                     "Chrome/79.0.3945.130 Safari/537.36"
 VARIOUS_ARTISTS = "5080"
